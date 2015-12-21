744674cc849bec8cfe2a5063cd0888a72ace4c16
ganmuren <daiweiyi@hotmail.com>
2015-12-21 17:24:42 +0800

    queue sort animation

--- js/asychSort.js
+++ js/asychSort.js
@@ -1,119 +1,176 @@
-$(function() {
-	$('#sortForm').bind('submit', function(e) {
-		e.preventDefault();
-		var val = $('#forSort').val();
-		var arr = val.split(',');
-		var obj = getSortModule();
-		var result = obj.sort(arr);
-		//$('#sortResult').html(JSON.stringify(result));
-	});
+$(function () {
+    $('#sortForm').bind('submit', function (e) {
+        e.preventDefault();
+        var val = $('#forSort').val();
+        var arr = val.split(',');
+        var obj = getSortModule();
+        var result = obj.sort(arr);
+        //$('#sortResult').html(JSON.stringify(result));
+    });
 });
 
 function getSortModule() {
-	var resultDiv = $('#sortResult');
-    var arr=[];
-	var removeItemAnimation=function(i){
-		var r=resultDiv.children().eq(i);
-		r.fadeOut('slow').queue(function(next){
-            r.remove();
-            next();
-        });
-	}
-	var showArray = function(onComplete) {
-		resultDiv.html("");
-		if (!$.isArray(arr)) return;
-		var ix = 0;
-        var $itemArr=[];
-        for(ix=0;ix<arr.length;ix++){
-            resultDiv.append('<span class="label label-default" style="display:none;">' + arr[ix] + '</span> ');
-        }
-
-        resultDiv.each(function(i){
-            var $k=$(this).children().eq(i);
-            $k.fadeIn('slow',function(){scheduleAnimation(++ix1);
-        });
-
-        var scheduleAnimation= function (ix1) {
-            if(ix1>=$itemArr.length){
+    var $resultDiv = $('#sortResult');
+    var arr = [];
+    var rmvItemAnimate = function (arr$item, onComplete) {
+        var rmv = function (ix) {
+            if (ix >= arr$item.length) {
                 onComplete();
                 return;
             }
-            $itemArr[ix1].fadeIn('slow',function(){scheduleAnimation(++ix1);})
+            arr$item[ix].fadeOut('slow', function () {
+                rmv(++ix);
+            });
         }
-        scheduleAnimation(0);//async
+        rmv(0);
+    };
+    var addItemAnimate = function (arr$item, onComplete) {
+        var add = function (ix) {
+            if (ix >= arr$item.length) {
+                onComplete();
+                return;
+            }
+            arr$item[ix].fadeIn('slow', function () {
+                add(++ix);
+            });
+        }
+        add(0);
+    };
+    var showArray = function (onComplete) {
+        $resultDiv.html("");
+        if (!$.isArray(arr)) return;
+        var ix = 0;
+        var $itemArr = [];
+        for (ix = 0; ix < arr.length; ix++) {
+            var $item = $('<span class="label label-default" style="display:none;">' + arr[ix] + '</span>');
+            $resultDiv.append($item);
+            $resultDiv.append(' ');
+            $itemArr.push($item);
+        }
+
+        addItemAnimate($itemArr, onComplete);
 
 
+    };
+    var swap = function (arr, i, j) {
+        var t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    };
+    var justifyIntArray = function (onComplete) {
+        var i = 0, arr$Rmv = [],
+            arr1 = [],
+            t;
+        if (arr.constructor !== Array) return;
 
+        while (i < arr.length) {
+            t = parseInt(arr[i])
+            if (!isNaN(t)) arr1.push(t);
+            else arr$Rmv.push($resultDiv.children().eq(i));
+            i++;
+        }
+        arr = arr1;
 
+        rmvItemAnimate(arr$Rmv, function () {
+            arr$Rmv.forEach(function ($item) {
+                $item.remove();
+            });
+            onComplete();
+        });
 
-		//var showItem = function() {
-		//	if (ix >= arr.length) {
-         //       return;
-         //   }
-		//	var c1 = '<span class="label label-default" style="display:none;">' + arr[ix] + '</span> ';
-		//	resultDiv.append(c1);
-		//	var c = resultDiv.children().last();
-		//	ix++;
-         //   $.queue();
-		//	c.fadeIn('slow').queue(function(){
-         //       showItem();
-         //   });
-		//}
-		//showItem();
+    };
+    var swapAnimate = function (arr$item, i, j, onComplete) {
+        var ti = arr$item[i].html();
+        var tj = arr$item[j].html();
 
-	}
-	var swap = function(arr, i, j) {
-		var t = arr[i];
-		arr[i] = arr[j];
-		arr[j] = t;
-	}
-	var justifyIntArray = function() {
-		var i = 0,
-			arr1 = [],
-			t;
-		if (arr.constructor !== Array) return;
-		
-		while (i < arr.length) {
-			t = parseInt(arr[i])
-			if (!isNaN(t)) arr1.push(t);
-			else removeItemAnimation(i);
-			i++;
-		}
-		return arr1;
-	}
-	var sort = function(arr1) {
-		var i, j;
-        //var cb= $.Callbacks();
-        arr=arr1;
-        //cb.add(showArray);
-        //cb.add(justifyIntArray);
-        //cb.fire();
-        //resultDiv.queue([showArray,justifyIntArray]);
-        //resultDiv.dequeue();
-        var funArr=[
-            function(next){showArray(function(){
+        var di = arr$item[i].css({color: 'red'}).fadeOut('slow').queue(function (next) {
+            $(this).html(tj);
+            next();
+        }).fadeIn('slow').queue(function (next) {
+            $(this).css({color: 'white'});
+            next();
+        });
+        var dj = arr$item[j].css({color: 'blue'}).delay(1200).fadeOut('slow').queue(function (next) {
+            $(this).html(ti);
+            next();
+        }).fadeIn('slow').queue(function (next) {
+            $(this).css({color: 'white'});
+            next();
+        });
+
+        $.when(di, dj).done(function () {
+            onComplete();
+        });
+       //var funcArr=[
+       //    function(next){
+       //        arr$item[i].css({color: 'red'}).fadeOut('slow').queue(function (next) {
+       //                    $(this).html(tj);
+       //                    next();
+       //                }).fadeIn('slow').queue(function (next) {
+       //                    $(this).css({color: 'white'});
+       //                });
+       //        next();},
+       //    function(next){
+       //        arr$item[j].css({color: 'blue'}).fadeOut('slow').queue(function (next) {
+       //                $(this).html(ti);
+       //                next();
+       //            }).fadeIn('slow').queue(function (next) {
+       //                $(this).css({color: 'white'});
+       //            });
+       //        onComplete();}
+       //];
+       // $.queue(funcArr);
+
+    };
+    var sortCore = function (arr$item, i, j, onComplete) {
+        if (arr.length < 2) return;
+        if (i >= arr.length - 1) {
+            if (onComplete)onComplete();
+            return;
+        }
+        if (j >= arr.length) {
+            i++;
+            j = i + 1;
+            sortCore(arr$item, i, j, onComplete);
+        }
+        if (arr[j] < arr[i]) {
+            swap(arr, i, j);
+            swapAnimate(arr$item, i, j, function () {
+                j++;
+                sortCore(arr$item, i, j, onComplete);
+            })
+        }
+    };
+    var sort = function (arr1, onComplete) {
+        var i, j;
+        arr = arr1;
+        var funArr = [
+            function (next) {
+                showArray(function () {
                     next();
                 })
             },
-            justifyIntArray
+            function (next) {
+                justifyIntArray(function () {
+                    next();
+                })
+            },
+            function (next) {
+                var arr$item = [];
+                $resultDiv.children().each(function (i) {
+                    arr$item.push($resultDiv.children().eq(i));
+                });
+                sortCore(arr$item, 0, 1, onComplete);
+            }
         ];
 
+        $resultDiv.queue(funArr);
+        //$resultDiv.dequeue('my1');
 
 
-        resultDiv.queue('my1',funArr);
-        resultDiv.dequeue('my1');
-        //showArray();
-		//arr = justifyIntArray(arr);
-		return;
-		if (arr.constructor !== Array) return;
-		if (arr.length < 2) return;
-		for (i = 0; i < arr.length - 1; i++)
-			for (j = i + 1; j < arr.length; j++)
-				if (arr[j] < arr[i])
-					swap(arr, i, j);
-		return arr;
-	}
-	return {
-		sort: sort
-	}
+        return arr;
+    };
+    return {
+        sort: sort
+    }
 }
